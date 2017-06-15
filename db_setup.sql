2d41b806172c92807e40dc3b3c18ba9e9018613c
Nicholas Spadaro <spadaro.nicholas@gmail.com>
2017-06-16 00:30:45 +0200

    fix db_setup.sql example rows

--- db_setup.sql
+++ db_setup.sql
@@ -18,10 +18,10 @@ CREATE TABLE public.item (
   useremail  varchar(250) NOT NULL
 );
 
-Insert into public.category (name, description) values ('E-cigarettes','Vaping stuff', 'spadaro.nicholas@gmail.com');
-Insert into public.category (name, description) values ('Consoles','Play stuff', 'spadaro.nicholas@gmail.com');
+Insert into public.category (name, description, useremail) values ('E-cigarettes','Vaping stuff', 'spadaro.nicholas@gmail.com');
+Insert into public.category (name, description, useremail) values ('Consoles','Play stuff', 'spadaro.nicholas2@gmail.com');
 
-Insert into public.item (name, description, category_id, date_insert) values ('Pico squeeze by Eleaf', 'compact mech mod bottom feeder', 1, '2017-06-13', 'spadaro.nicholas@gmail.com');
-Insert into public.item (name, description, category_id, date_insert) values ('Kangertech dripbox 160w', 'VW mod bottom feeder', 1, '2017-06-13', 'spadaro.nicholas@gmail.com');
-Insert into public.item (name, description, category_id, date_insert) values ('MS Xbox one s', '4k video by Microsoft', 2, '2017-06-13', 'spadaro.nicholas@gmail.com');
-Insert into public.item (name, description, category_id, date_insert) values ('PS4 Pro', 'PC style personalization by Sony', 2, '2017-06-13', 'spadaro.nicholas@gmail.com');
+Insert into public.item (name, description, category_id, date_insert, useremail) values ('Pico squeeze by Eleaf', 'compact mech mod bottom feeder', 1, '2017-06-13', 'spadaro.nicholas@gmail.com');
+Insert into public.item (name, description, category_id, date_insert, useremail) values ('Kangertech dripbox 160w', 'VW mod bottom feeder', 1, '2017-06-13', 'spadaro.nicholas@gmail.com');
+Insert into public.item (name, description, category_id, date_insert, useremail) values ('MS Xbox one s', '4k video by Microsoft', 2, '2017-06-13', 'spadaro.nicholas2@gmail.com');
+Insert into public.item (name, description, category_id, date_insert, useremail) values ('PS4 Pro', 'PC style personalization by Sony', 2, '2017-06-13', 'spadaro.nicholas2@gmail.com');
